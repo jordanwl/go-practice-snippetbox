3de8bcb8ad2e72099956d13577dbc458f8427363
Jordan Luong <jordanw.luong@gmail.com>
2021-06-01 15:00:26 +0900

    [feat] add createSnippet handler (CREATE)

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
+
 func showSnippet(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a specific snippet..."))
 }
@@ -18,8 +22,9 @@ func main() {
 	// register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/create", createSnippet)
 	mux.HandleFunc("/snippet", showSnippet)
-	
+
 	// Starts a new web server on port 4000. If http.ListenAndServe() returns an error,
 	// log error message and exit
 	log.Println("Starting server on :4000")
